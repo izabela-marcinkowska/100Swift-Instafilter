bb4557e06564d73651c757d3a461082abe945853
Devy <izabela.marcinkowska@outlook.com>
2024-10-28 08:27:41 +0100

    first commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Izabela Marcinkowska on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
